98ac2a54b7b7cfa6f99134efcfe4b63bb5207615
MSI\Gailbert <gailbert.huang@gmail.com>
2016-10-16 16:24:15 +0800

    Testing of overrid and serialize

--- Astral/Assets/Main/Resource/Script/NewBehaviourScript.cs
+++ Astral/Assets/Main/Resource/Script/NewBehaviourScript.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class NewBehaviourScript : MonoBehaviour 
+{
+	public bool Range;
+	public float Speed;
+	public int Damage;
+	[SerializeField] public Animation Anim;
+	[SerializeField] public string Walk;
+	[SerializeField] public string Run;
+	[SerializeField] public string Idle;
 
-	// Use this for initialization
-	void Start () {
-	
+	void Start()
+	{
+		Simula ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public virtual void Simula()
+	{
+		Range = false;
+		Speed = 1.5f;
+		Damage = 2;
+	}
+	public virtual void Update()
+	{
+		Debug.Log(Speed + " : " + Range + " : " + Speed);
 	}
 }
---
+++ Astral/Assets/Main/Resource/Script/NewBehaviourScript1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript1 : NewBehaviourScript 
+{
+	public override void Simula ()
+	{
+		base.Simula ();
+		Range = true;
+	}
+	public override void Update()
+	{
+		Debug.Log(Walk + " : " + Run + " : " + Idle + gameObject.name);
+	}
+}
