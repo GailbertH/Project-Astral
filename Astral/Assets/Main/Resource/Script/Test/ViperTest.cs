9ca3ea2b1a8538a241065b8cef1d3f2d1ef2a315
MSI\Gailbert <gailbert.huang@gmail.com>
2016-10-17 21:54:39 +0800

    its something..

---
+++ Astral/Assets/Main/Resource/Script/Test/ViperTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViperTest : MonoBehaviour 
+{
+	GameObject target;
+	void OnEnable()
+	{
+		target = GameObject.FindGameObjectWithTag ("Enemy");
+	}
+	void FindTarget()
+	{
+		
+	}
+	void GoToTarget()
+	{
+		
+	}
+}
